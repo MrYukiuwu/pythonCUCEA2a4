d1af725306ca160d04ac32459233c351274ac64c
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-22 14:42:51 -0600

    Trabajo 2,terminado

---
+++ ejercicioIF2.py
@@ -0,0 +1,10 @@
+
+Boleto = float (input("Ingresa el numero de tu boleto de loteria.   "))
+
+if Boleto == 1000 :
+    print ("Felicidades,eres el ganador")
+
+else :
+    print("Mucha suerte para la proxima. :D")
+    
+print("Muchas gracias por participar.")
