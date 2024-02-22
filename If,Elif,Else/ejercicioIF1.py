567065dbfd5417d7298f14511f70425fcc4bb5bc
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-22 15:54:06 -0600

    Trabajo 1 de if

---
+++ If,Elif,Else/ejercicioIF1.py
@@ -0,0 +1,8 @@
+numerouno = input("Ingrese un primer numero. ")
+numerodos = input("Ingrese un segundo numero. ")
+if numerouno > numerodos :
+    print("El numero mayor es el primero.")
+elif numerouno < numerodos :
+    print("El numero mayor es el segundo.")
+elif numerouno == numerodos :
+    print("Ambos numeros son iguales broh.")
