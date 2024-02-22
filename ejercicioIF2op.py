9197e467caccf839f2c88ca4ad84398a3e3ecc61
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-22 14:48:54 -0600

    Ejercicio 2,pero como operador ternario

---
+++ ejercicioIF2op.py
@@ -0,0 +1,6 @@
+Boleto = float (input("Ingresa el numero de tu boleto de loteria.   "))
+
+Estado = "Ganaste" if Boleto == 1000 else "suerte para la proxima unu"
+#Operador ternario
+
+print(Estado)
