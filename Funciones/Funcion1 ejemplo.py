099c677ae5c4c72a47b582ee219d71da1289f92c
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-27 15:35:34 -0600

    Ejercicios de funciones

--- Funciones/Funcion1 ejemplo.py
+++ Funciones/Funcion1 ejemplo.py
@@ -1,10 +1,18 @@
 nombre="Hugo" #Contexto global
+idioma="español"
 #El contexto global no puede alterar el contexto local.
-def saludo(): #Contexto local
-    nombre = "Yuki"
-    print(nombre)
-    print("Buen dia")
+#Para crear comentarios usas ctrl y }
+def saludo(nombrefuncion,idioma): #Contexto local
+    # nombre = "Yuki"
+    print(nombrefuncion)
+    print(f"Buen dia en {idioma}")
+    # print(nombre)
+    # print("Buen dia")
+    print("==================================================================================")
 #Solo cambia la variabl dentro de la funcion
-print(nombre)
-saludo()
-print(nombre)
+saludo(nombre,idioma)
+# si falta algun parametro,fallara
+print("==================================================================================")
+saludo("Pepe","Ingles")
+#Pones un parametro,asi que defines esa nueva variable,poniendola en el mismo lugar.
+saludo("Peblo","Suizo")
---
+++ Funciones/PracticaFuncion1.py
@@ -0,0 +1,16 @@
+def Sumota(numuno,numdos):
+    
+    print("Suma")
+    print(f"El resultado es {numuno + numdos}")
+    print("Estoy sumando en una funcion")
+    print("==================================================================================")
+def Multota(numuno,numdos):
+
+    print("Multiplicación")
+    print(f"El resultado multiplicado es {numuno * numdos}")
+    print("multiplicando con una funcion.")
+numuno= int(input("Inserte el primer numero.   "))
+numdos= int(input("Ahora el segundo.   "))
+Sumota(numuno,numdos)
+#Recomendable:todas las funcionas van hasta arriba,para que puedas minimizarlas.
+Multota(numuno,numdos)
