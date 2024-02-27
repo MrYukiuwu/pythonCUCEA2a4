dccae7eb084809001c2ce78bb8884b313714e256
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-27 14:41:53 -0600

    Editado casi todo,agregado notas sobre el uso de %

--- If,Elif,Else/ejercicioIF4.py
+++ If,Elif,Else/ejercicioIF4.py
@@ -1,9 +1,13 @@
-year = 0
 print("Bienvendiso al calculador de años biciestos 3000")
-year = input ("ingrese un año.")
+year = int(input ("ingrese un año."))
 #Debe ser divisible por 4,no por 100,solo si es 400
-if year:
-    print("Si es")
-elif year/100:
-    print("no")
-    #incompleto
+if year % 4 !=0:
+    #El % e una division,que te da el residuo.
+    print("No es")
+elif year % 4 == 0 and year % 100 !=0:
+    print("sies")
+elif year % 4 == 0 and year % 100 ==0: and year % 400 !=0:
+    print("noes")
+elif year % 4 == 0 and year % 100 ==0: and year % 400 ==0:
+    print("Sies")
+#incompleto
