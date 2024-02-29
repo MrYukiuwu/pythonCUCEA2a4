c094a1233638ebb0d54b3d8acb366716f429e0dd
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-29 14:29:54 -0600

    Eliminado

--- separador.py
+++
@@ -1,3 +0,0 @@
-cadena = "Hola mundo estamos explicando el funcionamiento de split en Python"
-palabras = cadena.split()
-print(palabras)  # Resultado: ['Hola', 'mundo', 'estamos', 'explicando', 'el', 'funcionamiento', 'de', 'split', 'en', 'Python']
