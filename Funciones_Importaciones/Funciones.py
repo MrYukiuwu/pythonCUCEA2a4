1e30050185e48ab4d9dce0ee7599829e829dd3c7
Hugo Antonio <hugoant.rolo@gmail.com>
2024-02-29 15:53:49 -0600

    Vimos como importar

--- Funciones_Importaciones/Funciones.py
+++ Funciones_Importaciones/Funciones.py
@@ -6,3 +6,12 @@ def suma(numero1,numero2):
 def multiplicar(numrero1,numero2):
     total= numrero1 + numero2
     return total
+
+import os
+def borrarPantalla(): #Definimos la función estableciendo el nombre que queramos
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system ("cls")
+
+#todo lo de arriba es para qu ela funcion de borrar pantalla sirva
---
+++ Tienda de manzanas/Mercado de verduras/ManzanasFunciones.py
@@ -0,0 +1,19 @@
+import funciones
+
+Descuento = 0
+Cantidad= float (input("Ingresa cuantas manzanas quieres vender. "))
+while Cantidad != 0:
+    Precio= float (input("Ingresa el precio de las manzanas. "))
+
+    descuentotes()
+
+    totalAPagar = Cantidad * Precio
+    
+    Cantidad= float (input("Ingresa cuantas manzanas quieres vender. "))
+    
+#Le falta :(
+    
+    
+    #Termina con la preguinta para ver si quieres romper el ciclo.
+    #Hay ota version,en donde usas if y break.
+print("Gracias por comprar,tqm.")
