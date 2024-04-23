b69770953601fc86f5d305cfc4fb4dd952daadac
Hugo Antonio <hugoant.rolo@gmail.com>
2024-04-23 16:00:32 -0600

    agregar usuarios

--- Sistema/usuarioControlador.py
+++ Sistema/usuarioControlador.py
@@ -49,3 +49,53 @@ def consultarDatosUnUsuario(codigo):
             cursor.close()
             connection.close()
             # print("MySQL connection is closed")
+ 
+
+#funcion para BORRAR un usuario        
+def BORRARDatosUnUsuario(codigo):
+    
+    datoABorrar = consultarDatosUnUsuario(codigo)
+    print(datoABorrar)
+    opcion = input(f"¿Estas seguro que deseas borrar el regitro? Y/N   ")
+    if opcion == "Y":
+        try:
+            connection = mysql.connector.connect(host='localhost',
+                                                database='zzeccso',
+                                                user='root',
+                                                password='')
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute(f"DELETE FROM usuario WHERE id_usuario = {codigo}")
+                print(f"Se borraron {cursor.rowcount} datos.")
+                print(cursor.rowcount)
+                connection.commit()
+                
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+            # print("MySQL connection is closed")
+    else:
+        print("No pasa nada uwu")
+
+#funcion para agregar datos
+def AGREAGARDatosUnUsuario(nombre, apellidoPaterno, apellidoMaterno, contrasenia, correo, telefono):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                            database='zzeccso',
+                                            user='root',
+                                            password='')
+        if connection.is_connected():
+        
+            cursor = connection.cursor()
+            cursor.execute(f"INSERT INTO `usuario` (`Id_usuario`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `contrasenia`, `correo`, `telefono`) VALUES ('{nombre}', '{apellidoPaterno}', '{apellidoMaterno}', '{contrasenia}', '{correo}', '{telefono}');")
+            connection.commit()
+           
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
--- Sistema/usuarioVista.py
+++ Sistema/usuarioVista.py
@@ -9,16 +9,29 @@ datos = usuarioControlador.consultarDatosUsuarios()
 #     modeloUsuario["apellidoPaterno"] = fila[2]
 #     modeloUsuario["apellidoMaterno"] = fila[3]
 #     print("Usuario Numero:" , modeloUsuario["usuario"] , modeloUsuario["nombre"] ,"Apellidos:",  modeloUsuario["apellidoPaterno"] , modeloUsuario["apellidoMaterno"])
-  #Esto recopila todos los datos
-#puedes seleccionar la funcion y ctrl+}
+  #TEXTO: Esto recopila todos los datos
+#TEXTO: puedes seleccionar la funcion y ctrl+}
 
+#TEXTO: esta solo registra el usuario con el id que tu pones aca arriba,es otra funcion distinta a la de arriba.
+# datos = usuarioControlador.consultarDatosUnUsuario(numero)
+# for fila in datos :
+    # modeloUsuario["usuario"] = fila[0]
+    # modeloUsuario["nombre"] = "@"+ fila[1]
+    # modeloUsuario["apellidoPaterno"] = fila[2]
+    # modeloUsuario["apellidoMaterno"] = fila[3]
+    # print("Usuario Numero:" , modeloUsuario["usuario"] , modeloUsuario["nombre"] ,"Apellidos:",  modeloUsuario["apellidoPaterno"] , modeloUsuario["apellidoMaterno"])
 
-numero = input("inserte numero de id a buscar.    ")
-datos = usuarioControlador.consultarDatosUnUsuario(numero)
-#esta solo registra el usuario con el id que tu pones aca arriba,e otra funcion distinta a la de arriba.
-for fila in datos :
-    modeloUsuario["usuario"] = fila[0]
-    modeloUsuario["nombre"] = "@"+ fila[1]
-    modeloUsuario["apellidoPaterno"] = fila[2]
-    modeloUsuario["apellidoMaterno"] = fila[3]
-    print("Usuario Numero:" , modeloUsuario["usuario"] , modeloUsuario["nombre"] ,"Apellidos:",  modeloUsuario["apellidoPaterno"] , modeloUsuario["apellidoMaterno"])
+# numero = input("inserte numero de id a buscar.    ")
+# usuarioControlador.BORRARDatosUnUsuario(numero)
+#TEXTO: funcion para BORRAR
+
+INS = input("¿Deseas insertar un usuario? Y/N   ")
+if INS == "Y":
+    Nombre = input("Inserte nombre.   ")
+    AP = input("Inseerte apellido PATERNO.    ")
+    AM = input("Inserte apellido MATERNO.    ")
+    PW = input("inserte Contraseña.    ")
+    Mail = input("inserte correo.    ")
+    celu = input("inserte numero de telefono.    ")
+    
+    usuarioControlador.AGREAGARDatosUnUsuario(Nombre,AP,AM,PW,Mail,celu)
